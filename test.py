74fd9fe4ea300ed45f572a6c473b56126ab38b11
codingBoyEveryday <zhouhanrongtue@gmail.com>
2017-02-10 11:29:00 +0100

    generate datasheet in excel directly

--- test.py
+++ test.py
@@ -1,42 +1,61 @@
 from Fit20Exercise import *  
 import datetime
+import xlsxwriter
 
 print 'Please input the file name: '
 filename = raw_input('')
-f = open(filename+'.txt', 'w')
-#f = open(filename+'.xlsx','w')
+workbook = xlsxwriter.Workbook(filename + '.xlsx')
+worksheet = workbook.add_worksheet()
 
-# a = NewConnectionTest(mac_addr = '34:B1:F7:D0:5A:02', iface = 0)
-# print('Connecting to EC401')
+time_now = datetime.datetime.now()
+t = time_now.strftime('%H:%M:%S')
+
+
 a = NewConnectionTest(mac_addr = '7C:EC:79:E3:97:F6', iface = 0)
 print('Connecting to EC129')
 b = NewConnectionTest(mac_addr = '7C:EC:79:E4:49:12', iface = 1)
 print('Connecting to EC140')
-#c = NewConnectionTest(mac_addr = '7C:EC:79:E4:5A:EC', iface = 2)
-#print('Connecting to EC100')
-#d = NewConnectionTest(mac_addr = '7C:EC:79:E3:9A:AF', iface = 3)
-#print('Connecting to EC125')
-#b = NewConnectionTest(mac_addr = '7C:EC:79:E4:49:66', iface = 1)
-#print('Connecting to EC120')
+# c = NewConnectionTest(mac_addr = '7C:EC:79:E4:5A:EC', iface = 2)
+# print('Connecting to EC100')
+# d = NewConnectionTest(mac_addr = '7C:EC:79:E3:9A:AF', iface = 3)
+# print('Connecting to EC125')
+# b = NewConnectionTest(mac_addr = '7C:EC:79:E4:49:66', iface = 1)
+# print('Connecting to EC120')
 
-for i in range(1, 500):
-    print 'ec000 ', a.get_data()
-    f.write(str(datetime.datetime.now()) +',ec129,' + str(a.get_data()) + ' ')
-    print 'ec001 ', b.get_data()
-    f.write(str(datetime.datetime.now()) + ',ec140,' + str(b.get_data()) + ' ')
-    #print 'ec000 ', a.get_data()
-    #f.write(str(datetime.datetime.now()) + ',ec401,' + str(a.get_data()) + ' ')
+# Start from the first cell. Rows and columns are zero indexed.
+row = 0
+col = 0
+
+for i in range(1, 50):
+    print 'ec129 ', a.get_data()
+    # f.write(str(datetime.datetime.now()) +',ec129,' + str(a.get_data()) + ' ')
+    worksheet.write(row, col, str(t))
+    worksheet.write(row, col + 1, 'ec129')
+    worksheet.write(row, col + 2, str(a.get_data()))
+
+    print 'ec140 ', b.get_data()
+    # f.write(str(datetime.datetime.now()) + ',ec140,' + str(b.get_data()) + ' ')
+    worksheet.write(row, col + 3, str(t))
+    worksheet.write(row, col + 4, 'ec140')
+    worksheet.write(row, col + 5, str(b.get_data()))
+    # print 'ec000 ', c.get_data()
+    # f.write(str(datetime.datetime.now()) + ',ec401,' + str(c.get_data()) + ' ')
     # print 'ec001 ', d.get_data()
-    #f.write(str(datetime.datetime.now()) + ',ec125,' + str(d.get_data()) + ' ')
-#    print 'ec000 ', b.get_data()
-#    f.write(str(datetime.datetime.now()) + ',ec120,' + str(b.get_data()))
-    f.write("\n")
+    # f.write(str(datetime.datetime.now()) + ',ec125,' + str(d.get_data()) + ' ')
+    # print 'ec000 ', e.get_data()
+    # f.write(str(datetime.datetime.now()) + ',ec120,' + str(e.get_data()))
+    # f.write("\n")
+
+    row += 1
+
+
+
+
 
 a.disconnect()
 b.disconnect()
-#c.disconnect()
-#d.disconnect()
-#e.disconnect()
+# c.disconnect()
+# d.disconnect()
+# e.disconnect()
 
-f.close()
-print "Testing is finished !"
+workbook.close()
